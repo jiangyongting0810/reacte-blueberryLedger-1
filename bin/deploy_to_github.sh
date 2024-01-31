081c2b972b2e34afca7d5c9633d9db72611ee6f2
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-01-31 20:33:34 +0800

    update

--- bin/deploy_to_github.sh
+++ bin/deploy_to_github.sh
@@ -5,6 +5,5 @@ cd dist
 git init
 git add .
 git commit -m deploy
-git remote add origin git@github.com:jiangyongting0810/reacte-blueberryLedger-1-Preview.git
-git push -f origin main
+git push -f origin main:main
 cd -
