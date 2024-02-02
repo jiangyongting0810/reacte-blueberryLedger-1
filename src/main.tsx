f3a6a69ace223a611f4a303fe0c4cb65f1840e15
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-02-02 20:18:57 +0800

    添加四个路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import ErrorPage from './components/Errorpage'
+import { NavLink, Outlet, Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RedirectToWelcome1 } from './components/RedirectToWelcome1'
 
 window.console.log(Router)
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
-    errorElement: <ErrorPage/>,
+    element: <div>Hello world!<Outlet/></div>,
+    errorElement: <RedirectToWelcome1/>,
+    children: [
+      { index: true, element: <div>空</div> },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          { index: true, element: <div>空</div> },
+          {
+            path: '1',
+            element: (
+              <div> 1 <NavLink to="/welcome/2">下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '2',
+            element: (
+              <div> 2 <NavLink to="/welcome/3">下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '3',
+            element: (
+              <div> 3 <NavLink to="/welcome/4">下一页</NavLink> </div>
+            )
+          },
+          {
+            path: '4',
+            element: (
+              <div> 4 <NavLink to="/xxx">开始记账</NavLink> </div>
+            )
+          },
+        ]
+      }
+    ],
   },
 ])
 
