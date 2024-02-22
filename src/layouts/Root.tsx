f6e20d483eb21a18ca547d5a1661fce350f769e1
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-02-22 15:43:13 +0800

    使用 Root 组件默认展示 /welcome/1

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,6 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom'
 
-export const MainLayout: React.FC = () => {
+export const Root: React.FC = () => {
   const hasRead = localStorage.getItem('hasReadWelcome')
   if (hasRead === 'yes') {
     return <Navigate to='/home'/>
