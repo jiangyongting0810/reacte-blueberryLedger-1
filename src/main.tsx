8bd5bcf67f30e7333f3337317cfc0c0f8cae8a40
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-02-26 11:16:25 +0800

    隐藏多余log

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import 'uno.css'
 import './global.scss'
 
-window.console.log(Router)
+// window.console.log(Router)
 
 const div = document.getElementById('root') as HTMLElement
 
