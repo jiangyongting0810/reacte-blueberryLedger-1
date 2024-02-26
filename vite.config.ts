b2b9bfdb748cc2305e1076ec8cfdb2fa2d81df40
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-02-26 14:07:59 +0800

    使用 vite-plugin-mock 在本地创建 mock 服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command }) => ({
   plugins: [
     Unocss(),
-    react()
+    react(),
+    viteMockServe()
   ]
-})
+}))
