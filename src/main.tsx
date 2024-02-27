a8580bc157665bb35457bfcb35db86a10223263c
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-02-27 17:19:41 +0800

    使用 import 确定 css 顺序

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
-import 'uno.css'
 import './global.scss'
+import 'virtual:uno.css'
+import './app.scss'
 import 'virtual:svgsprites'
 // window.console.log(Router)
 
