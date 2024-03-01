f3bd545de597764ba1ad799fd638de81113119f8
jiangyongting0810 <jiangyongting0810@gmail.com>
2024-03-01 15:07:28 +0800

    使用 useMenuStore 来封装 Menu 的状态

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,5 +1,4 @@
-import { useContext } from 'react'
-import { menuContext } from '../contexts/menuContext'
+import { useMenuStore } from '../stores/useMenuStore'
 import { Icon } from './Icon'
 
 interface Props {
@@ -7,11 +6,11 @@ interface Props {
 }
 
 export const TopNav: React.FC<Props> = ({ title = '蓝莓记账' }) => {
-  const { setVisible } = useContext(menuContext)
+  const { visible, setVisible } = useMenuStore()
   return (
     <div text-white flex items-center pt-24px pb-8px px-24px>
       <Icon name="menu" className="w-24px h-24px mr-16px"
-        onClick={() => { setVisible(true) }}
+        onClick={() => { setVisible(!visible) }}
       />
       <h1 text-24px>{title}</h1>
     </div>
